b274385ee561fbfcd1fcde92c0cc38b4620f0e3d
aliyasserdev-pixel <aliyasser.dev@gmail.com>
2026-06-21 09:29:44 +0200

    update project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-country-explorer/",
   plugins: [react()],
+  base: "/react-country-explorer/",
 });
